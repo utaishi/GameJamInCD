78fee2d9fb0c8932239ad37871fa2bff8943b771
Soeking <mr.kaito01@gmail.com>
2019-03-02 22:22:44 +0900

    3/2

--- Assets/Zekki/Script/SetNotes.cs
+++ Assets/Zekki/Script/SetNotes.cs
@@ -2,15 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SetNotes : MonoBehaviour {
+public class SetNotes : MonoBehaviour
+{
 
-	// Use this for initialization
+	public char[] firstNotes = new char[6];
+	public char[] secondNotes = new char[6];
+	public char[] thirdNotes = new char[6];
+	private char[] elements = {'a', 's', 'd'};
+	public GameObject[] notes;
+
 	void Start () {
-		
+		for (int i = 0; i < 6; i++)
+		{
+			firstNotes[i] = elements[Random.Range(0, 3)];
+			secondNotes[i] = elements[Random.Range(0, 3)];
+			thirdNotes[i] = elements[Random.Range(0, 3)];
+		}
 	}
 	
-	// Update is called once per frame
 	void Update () {
+		
+	}
+
+	void setImage(char[] parameters)
+	{
 		
 	}
 }
