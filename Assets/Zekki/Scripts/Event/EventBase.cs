2d84ce968cc4bcab41bfd07d97bfb0a3a65f33b2
Doo <doo11gms@gmail.com>
2019-03-02 23:30:57 +0900

    add result scene

---
+++ Assets/Zekki/Scripts/Event/EventBase.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Zekki.Event
+{
+    public abstract class EventBase : MonoBehaviour
+    {
+        [SerializeField] bool m_ExecuteOnAwake = true;
+
+        private void Awake()
+        {
+            if (m_ExecuteOnAwake) Execute();
+        }
+
+        public abstract void Execute();
+    }
+}
---
+++ Assets/Zekki/Scripts/Event/SceneChangeEvent.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Zekki.Event
+{
+    public class SceneChangeEvent : EventBase
+    {
+        [SerializeField] string m_NextScene;
+
+        public override void Execute()
+        {
+            if (m_NextScene == "") throw new System.Exception("The name of next scene is null.");
+
+            SceneManager.LoadScene(m_NextScene);
+        }
+    }
+}
