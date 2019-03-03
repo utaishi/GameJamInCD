57a0b29375957469bf97a0810b269fb1bb5dfce1
Tim Olson <mr.kaito01@gmail.com>
2019-03-03 17:25:52 +0900

    notes set random

--- Assets/Zekki/Script/SetNotes.cs
+++ Assets/Zekki/Script/SetNotes.cs
@@ -10,6 +10,9 @@ public class SetNotes : MonoBehaviour
 	public char[] thirdNotes = new char[6];
 	private char[] elements = {'a', 's', 'd'};
 	public GameObject[] notes;
+	public GameObject onCymbal;
+	public GameObject onFrypan;
+	public GameObject onHarisen;
 
 	void Start () {
 		for (int i = 0; i < 6; i++)
@@ -18,14 +21,30 @@ public class SetNotes : MonoBehaviour
 			secondNotes[i] = elements[Random.Range(0, 3)];
 			thirdNotes[i] = elements[Random.Range(0, 3)];
 		}
+		SetImage(firstNotes);
 	}
 	
 	void Update () {
 		
 	}
 
-	void setImage(char[] parameters)
+	void SetImage(char[] parameters)
 	{
-		
+		for (int i = 0; i < 6; i++)
+		{
+			switch (parameters[i])
+			{
+				case 'a':
+					notes[i] = Instantiate(onCymbal,notes[i].transform);
+					break;
+				case 's':
+					notes[i] = Instantiate(onFrypan,notes[i].transform);
+					break;
+				case 'd':
+					notes[i] = Instantiate(onHarisen,notes[i].transform);
+					break;
+					
+			}
+		}
 	}
 }
