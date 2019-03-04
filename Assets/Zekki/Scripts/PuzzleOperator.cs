dfd433834647aa2ce23d3815aa6ae135bdd6ff7f
siro53 <shusuke.shirotake@me.com>
2019-03-04 22:08:05 +0900

    キーボード操作できるように改修中

--- Assets/Zekki/Scripts/PuzzleOperator.cs
+++ Assets/Zekki/Scripts/PuzzleOperator.cs
@@ -6,9 +6,16 @@ using UnityEngine;
 
 public class PuzzleOperator : MonoBehaviour {
 
-    public GameObject[,] pieces =  new GameObject[4, 4]; // パズルピース
+    public static GameObject[,] pieces =  new GameObject[4, 4]; // パズルピース
     int[] x = new int[] { 0, 1, 2, 3 };
     int[] y = new int[] { 0, 1, 2, 3 };
+
+    static void Swap<T>(ref T a, ref T b)
+    {
+        var t = a;
+        a = b;
+        b = t;
+    }                                                                          
 
     void Start()
     {
@@ -23,13 +30,87 @@ public class PuzzleOperator : MonoBehaviour {
             {
                 number = 4 * i + j;
                 pieces[i, j] = GameObject.Find("PuzzlePieces_" + number.ToString());
-                pieces[i, j].transform.localPosition = new Vector3(x[i], y[j], 0);
+                pieces[i, j].transform.localPosition = new Vector3(x[i], y[j]);
             }
         }
     }
 
+    int selectX = 0;
+    int selectY = 0;
+    public GameObject emptyPieces = GameObject.Find("PuzzlePieces_14");
+
     void Update ()
     {
         //十字キーでカーソル移動、Spaceキーで移動
+        // PuzzlePieces_14はSprite Rendererをオフにしている
+        // そのためPuzzlePieces_14(pieces[3, 2])とその周囲をswapすればよい
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            for(int i = 0; i < 4; i++)
+            {
+                for(int j = 0; j < 4; j++)
+                {
+                    if(pieces[i, j].transform.localPosition == Vector3.up + emptyPieces.transform.localPosition)
+                    {
+                        if (Input.GetKeyDown(KeyCode.Space))
+                        {
+                            Debug.Log("up");
+                            Swap(ref pieces[i, j], ref emptyPieces);
+                        }
+                    }
+                }
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    if (pieces[i, j].transform.localPosition == Vector3.down + emptyPieces.transform.localPosition)
+                    {
+                        if (Input.GetKeyDown(KeyCode.Space))
+                        {
+                            Debug.Log("down");
+                            Swap(ref pieces[i, j], ref emptyPieces);
+                        }
+                    }
+                }
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    if (pieces[i, j].transform.localPosition == Vector3.right + emptyPieces.transform.localPosition)
+                    {
+                        if (Input.GetKeyDown(KeyCode.Space))
+                        {
+                            Debug.Log("right");
+                            Swap(ref pieces[i, j], ref emptyPieces);
+                        }
+                    }
+                }
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    if (pieces[i, j].transform.localPosition == Vector3.left + emptyPieces.transform.localPosition)
+                    {
+                        if (Input.GetKeyDown(KeyCode.Space))
+                        {
+                            Debug.Log("left");
+                            Swap(ref pieces[i, j], ref emptyPieces);
+                        }
+                    }
+                }
+            }
+        }
     }
 }
