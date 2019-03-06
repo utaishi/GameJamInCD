e32b57756c5438b4dcb49d8adfde9c1fb2a7da3a
utaishi <taishi.uemura2@gmail.com>
2019-03-06 22:07:22 +0900

    進捗

---
+++ Assets/Zekki/Scripts/InputSpace.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using UniRx.Triggers;
+
+namespace Zekki
+{ 
+    public class InputSpace : MonoBehaviour {
+        [SerializeField] private ChangeSceneUeta cs;
+        [SerializeField] private string nextScene;
+	// Use this for initialization
+	void Start () {
+            this.UpdateAsObservable()
+                    .Subscribe(_ =>
+                    {
+                        if (Input.GetKeyDown(KeyCode.Space))
+                        {
+                            cs.ChangeAnyScene(nextScene);
+                        }
+
+
+                    }).AddTo(this);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
+}
