a9c8fac70b192b1cf5f64e95d251920af54f4c93
siro53 <shusuke.shirotake@me.com>
2019-03-07 23:50:02 +0900

    add timer

---
+++ Assets/Zekki/Scripts/TimerOnOrdinaceGamePart.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerOnOrdinaceGamePart : MonoBehaviour {
+
+    private Text timeText;
+    public static float time = TimerOnRhythmPart.time;
+    public static float passTime = TimerOnRhythmPart.passTime;
+
+	void Start () {
+        timeText = GetComponent<Text>();
+        timeText.text = time.ToString("f2");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        time -= Time.deltaTime;
+        timeText.text = time.ToString("f2");
+
+        if(time <= 0)
+        {
+            Debug.Log("GAMEOVER");
+        }
+	}
+}
